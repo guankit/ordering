c0e7c94c59f94ecd1357ad49d1c0b73e21ff39fb
yyw1227 <865465569@qq.com>
2019-03-11 08:39:58 +0800

    首页布局基本完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,114 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+const app = getApp();
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    num:[
-      {num:1,name:'hah'},
-      { num: 2, name: 'hah' }
-    ]
+    menu: [
+      { date: '2019-02-18 星期一', name: '红烧狮子球、油闷茄子' },
+      { date: '2019-02-18 星期二', name: '红烧狮子球、油闷茄子' },
+      { date: '2019-02-18 星期三', name: '红烧狮子球、油闷茄子' },
+      { date: '2019-02-18 星期四', name: '红烧狮子球、油闷茄子、韭菜虾米、鱼香肉丝' },
+      { date: '2019-02-18 星期五', name: '红烧狮子球、油闷茄子' },
+      { date: '2019-02-18 星期五', name: '红烧狮子球、油闷茄子' },
+      { date: '2019-02-18 星期五', name: '红烧狮子球、油闷茄子' },
+      { date: '2019-02-18 星期五', name: '红烧狮子球、油闷茄子' },
+      { date: '2019-02-18 星期五', name: '红烧狮子球、油闷茄子' },
+      { date: '2019-02-18 星期五', name: '红烧狮子球、油闷茄子' },
+    ],
+    menu_height: 0,
+    showQr:false,
+    showOrder:false,
+    number:1,
+    price:12
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  orderHandle: function () {
+    this.setData({
+      showOrder: true
     })
   },
-  add:function(){
-    let name = this.data.num[0].name;
-    let str = 'num[0].name'
+  qrHandle: function () {
     this.setData({
-      str:name+'+h'
+      showQr: true
+    })
+  },
+  colseModal:function(){
+    this.setData({
+      showQr: false,
+      showOrder: false
+    })
+  },
+  onChange:function(e){
+    this.setData({
+      number: e.detail,
+      price:12*e.detail
     });
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  // 禁止屏幕滚动
+  preventTouchMove: function () {
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          menu_height: res.windowHeight * 750 / res.windowWidth - 420
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
